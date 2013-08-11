5ccb00110cc9f2619cb98366636f9ef265837690
Harry Marr <harry.marr@gmail.com>
2013-08-11 17:31:53 +0100

    Server funcs for starting stacks, services, and processes

--- rigd/rigd.go
+++ rigd/rigd.go
@@ -21,20 +21,17 @@ type ProcessConfig struct {
 }
 
 func main() {
-	//launchServer()
-
 	if len(os.Args) < 2 {
 		fmt.Fprintf(os.Stderr, "usage: %v configdir\n", os.Args[0])
 		os.Exit(1)
 	}
-	config := NewConfigFromDir(os.Args[1])
 
-	for _, s := range(config.Stacks) {
-		s.Start()
-	}
+	launchServer(os.Args[1])
 }
 
-func launchServer() {
+func launchServer(configDir string) {
+	config := NewConfigFromDir(configDir)
+
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, os.Kill, os.Signal(syscall.SIGTERM))
 	go func() {
@@ -43,7 +40,7 @@ func launchServer() {
 		os.Exit(0)
 	}()
 
-	srv, err := NewServer()
+	srv, err := NewServer(config.Stacks)
 	if err != nil {
 		log.Fatal(err)
 	}
--- rigd/server.go
+++ rigd/server.go
@@ -1,14 +1,65 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gocardless/rig"
 )
 
-type Server struct{}
+type Server struct {
+	stacks map[string]*Stack
+}
 
-func NewServer() (*Server, error) {
-	srv := &Server{}
+func NewServer(stacks map[string]*Stack) (*Server, error) {
+	srv := &Server{stacks: stacks}
 	return srv, nil
+}
+
+func (srv *Server) StartStack(stack string) error {
+	s := srv.stacks[stack]
+	if s == nil {
+		return fmt.Errorf("stack '%v' does not exist", stack)
+	}
+
+	go s.Start()
+
+	return nil
+}
+
+func (srv *Server) StartService(stack, service string) error {
+	s := srv.stacks[stack]
+	if s == nil {
+		return fmt.Errorf("stack '%v' does not exist", stack)
+	}
+
+	svc := s.Services[service]
+	if svc == nil {
+		return fmt.Errorf("service '%v' does not exist", service)
+	}
+
+	go svc.Start()
+
+	return nil
+}
+
+func (srv *Server) StartProcess(stack, service, process string) error {
+	s := srv.stacks[stack]
+	if s == nil {
+		return fmt.Errorf("stack '%v' does not exist", stack)
+	}
+
+	svc := s.Services[service]
+	if svc == nil {
+		return fmt.Errorf("service '%v' does not exist", service)
+	}
+
+	p := svc.Processes[process]
+	if p == nil {
+		return fmt.Errorf("process '%v' does not exist", process)
+	}
+
+	go p.Start(svc.Dir)
+
+	return nil
 }
 
 func (srv *Server) Version() rig.ApiVersion {
