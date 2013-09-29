8b447e612a845a0e6980631b19bd8a18cfadc87f
Steve <steve@stevedomin.com>
2013-09-29 17:31:33 +0100

    Adds ExpandPath utils
    Expand path like ~/test and ~user/test

--- utils/utils.go
+++ utils/utils.go
@@ -4,6 +4,9 @@ import (
 	"crypto/rand"
 	"encoding/hex"
 	"io"
+	"os/user"
+	"regexp"
+	"strings"
 )
 
 // https://github.com/dotcloud/docker/blob/940d58806c3e3d4409a7eee4859335e98139d09f/image.go#L218-225
@@ -14,4 +17,32 @@ func GenerateId() string {
 		panic(err) // This shouldn't happen
 	}
 	return hex.EncodeToString(id)
+}
+
+var expandPathRegExp = regexp.MustCompile(`^~(\w*)`)
+
+func ExpandPath(path string) string {
+	matches := expandPathRegExp.FindAllStringSubmatch(path, -1)
+	if len(matches) <= 0 {
+		return path
+	}
+
+	replace := matches[0][0]
+	username := matches[0][1]
+
+	var usr *user.User
+	var err error
+	if username != "" {
+		usr, err = user.Lookup(username)
+	} else {
+		usr, err = user.Current()
+	}
+	if err != nil {
+		return path
+	}
+
+	homeDir := usr.HomeDir
+	path = strings.Replace(path, replace, homeDir, 1)
+
+	return path
 }
